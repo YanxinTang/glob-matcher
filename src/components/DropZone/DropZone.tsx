74ba9a9e7cfb8eaf2ae245cc1e2c21bb077491ba
tyx1703 <tyx1703@gmail.com>
2020-03-19 08:36:53 +0800

    feat: traverse file tree

--- src/components/DropZone/DropZone.tsx
+++ src/components/DropZone/DropZone.tsx
@@ -17,11 +17,72 @@ function DropZone() {
     event.preventDefault();
   }
 
-  const dropHandler = (event: React.DragEvent<HTMLDivElement>) => {
+  const dropHandler = async (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
     const df = event.dataTransfer;
+    const tree = await readDataTransfer(df);
+    console.log(tree)
+    setActive(false);
+  }
 
-    console.log(event)
+ 
+
+  type FileType = {
+    name: string,
+    path: string,
+    type: 0,
+  }
+
+  type DirType = {
+    name: string,
+    path: string,
+    type: number,
+    children: Array<FileOrDir>,
+  }
+
+  type FileOrDir = FileType | DirType;
+
+  async function readDataTransfer(df: DataTransfer) : Promise<Array<FileOrDir>> {
+    const traverseFileTreePromises: Array<Promise<FileOrDir | undefined>> = [];
+    for (var i = 0; i < df.items.length; i++) {
+      var entry = df.items[i].webkitGetAsEntry();
+      traverseFileTreePromises.push(traverseFileTree(entry));
+    }
+    const result = await Promise.all(traverseFileTreePromises);
+    return  result.filter<FileOrDir>((tree): tree is FileOrDir => tree !== undefined);
+  }
+
+  async function traverseFileTree(entry: any, path: string = '/') : Promise<FileOrDir | undefined> {
+    if (entry.isFile) {
+      return await new Promise<FileType>((resolve) => {
+        entry.file((file: any) => {
+          resolve({
+            name: file.name,
+            path: path,
+            type: 0,
+          })
+        });
+      });
+    } else if (entry.isDirectory) {
+      return await new Promise<DirType>((resolve) => {
+        var dirReader = entry.createReader();
+        dirReader.readEntries(async (entries: any) => {
+          const result: DirType = {
+            name: entry.name,
+            path: path,
+            type: 1,
+            children: [],
+          };
+          for (var i = 0; i < entries.length; i++) {
+            const tree = await traverseFileTree(entries[i], path + entry.name);
+            if (tree) {
+              result.children.push(tree)
+            }
+          }
+          resolve(result);
+        });
+      });
+    }
   }
 
   return (
